2ca52200771e44047139eedfd2a4ca35d73db58a
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-19 01:39:57 +0600

    class 8 done

--- class8.js
+++ class8.js
@@ -1,1 +1,10 @@
 // dot vs bracket notation
+const person = {
+    nam: 'hero',
+    age_o:50,
+    friends: ['d','e','dd'],
+    10: 'code'
+}
+console.log(person.nam)
+console.log(person['age_o'])
+console.log(person[10])
