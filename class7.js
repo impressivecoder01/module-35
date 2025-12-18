beeceb0398d40a75ec707aeae5422af98f387ff8
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-19 01:35:28 +0600

    class seven done

--- class7.js
+++ class7.js
@@ -1,1 +1,15 @@
 // explore local storage and session storage with json
+// console.log('sss')
+const handle =()=>{
+    const nam = document.getElementById('name').value
+    const email = document.getElementById('email').value
+    // console.log(nam)
+    // localStorage.setItem(nam,email)
+    const data = {email,nam}
+    localStorage.setItem('new',JSON.stringify(data))
+}
+const item = localStorage.getItem('new')
+console.log(JSON.parse(item))
+const r = ()=>{
+    localStorage.clear()
+}
